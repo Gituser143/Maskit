f75dddd7a34631c7ab277bd21a73b3fd0a768c7e
Gituser143 <bhargavsnv100@gmail.com>
2020-10-25 04:17:34 +0530

    Create pseudo workflow for client

--- client.py
+++ client.py
@@ -19,40 +19,62 @@ def captureImage():
     os.system(cmd)
 
 
-# Capture image
-captureImage()
+def sendImage(serverIP, serverPort):
 
-# Connect to server
-s = socket.socket()
-s.connect((serverIP, serverPort))
-print("Connected to server")
+    # Connect to server
+    s = socket.socket()
+    s.connect((serverIP, serverPort))
+    print("Connected to server")
 
-# Send image
-print("Sending image")
-f = open("image.jpg", "rb")
-line = f.read(1024)
-while (line):
-    s.send(line)
+    # Send image
+    print("Sending image")
+    f = open("image.jpg", "rb")
     line = f.read(1024)
+    while (line):
+        s.send(line)
+        line = f.read(1024)
 
-print("Image sent")
+    print("Image sent")
 
-# Close connection
-s.close()
+    # Close connection
+    s.close()
 
-# Create server to receive classification
-s = socket.socket()
-s.bind((clientIP, clientPort))
-s.listen(1)
-print("Accepting Connections")
 
-# Accept connection
-sc, address = s.accept()
+def receiveClass(clientIP, clientPort):
 
-# Receive response
-print("Received connection")
-line = sc.recv(1024)
-print(line.decode())
+    # Create server to receive classification
+    s = socket.socket()
+    s.bind((clientIP, clientPort))
+    s.listen(1)
+    print("Accepting Connections")
 
-# Close connection
-sc.close()
+    # Accept connection
+    sc, address = s.accept()
+
+    # Receive response
+    print("Received connection")
+    line = sc.recv(1024)
+    mask = line.decode()
+    print("Message:", mask)
+
+    # Close connection
+    sc.close()
+
+    return mask
+
+
+while(1):
+    # Scan RFID
+
+    # If RFID is valid
+    # Capture image
+    captureImage()
+
+    # Send image to server
+    sendImage(serverIP, serverPort)
+
+    # Recive classification
+    mask = receiveClass(clientIP, clientPort)
+
+    # If mask
+    # Rotate motor to open door
